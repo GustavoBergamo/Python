fb09753f938b97bea9e02fcb779c642ca1a7812f
Gustavo bergamo <106891173+GustavoBergamo@users.noreply.github.com>
2022-07-23 17:18:47 -0300

    Update Tabuada.py

--- Tabuada.py
+++ Tabuada.py
@@ -1,21 +1,11 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2513
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 HelveticaNeue;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\deftab560
-\pard\pardeftab560\slleading20\pardirnatural\partightenfactor0
-
-\f0\fs24 \cf0 n=int(input('Voc\'ea quer saber a tabuada de qual n\'famero? '))\
-print (n,'x 1 =',n)\
-print (n,'x 2 =',n*2)\
-print (n,'x 3 =',n*3)\
-\pard\pardeftab560\slleading20\pardirnatural\partightenfactor0
-\cf0 print (n,'x 4 =\'91,n*4)\
-print (n,'x 5 =\'91,n*5)\
-print (n,'x 6 =\'91,n*6)\
-print (n,'x 7 =\'91,n*7)\
-print (n,'x 8 =\'91,n*8)\
-print (n,'x 9 =\'91,n*9)\
-print (n,'x 10 =\'91,n*10)\
-}
+n=int(input('Você quer saber a tabuada de qual número? '))
+print (n,'x 1 = ',n)
+print (n,'x 2 = ',n*2)
+print (n,'x 3 = ',n*3) 
+print (n,'x 4 = ',n*4)
+print (n,'x 5 = ',n*5)
+print (n,'x 6 = ',n*6)
+print (n,'x 7 = ',n*7)
+print (n,'x 8 = ',n*8)
+print (n,'x 9 = ',n*9)
+print (n,'x 10 =',n*10)
