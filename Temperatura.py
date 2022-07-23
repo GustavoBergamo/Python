cf23f0ecaf67ed48d7c1fd76693deedd184f467d
Gustavo bergamo <106891173+GustavoBergamo@users.noreply.github.com>
2022-07-23 17:09:41 -0300

    novas adições

---
+++ Temperatura.py
@@ -0,0 +1,11 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2513
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 HelveticaNeue;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab560
+\pard\pardeftab560\slleading20\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 c = float(input('Qual a temperatura em celcius? '))\
+f = ((9 * c)/5) + 32\
+print('Em Farenheit \'e9 \{:.2f\}'.format (f))}
---
+++ Tinta.py
@@ -0,0 +1,13 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2513
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 HelveticaNeue;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab560
+\pard\pardeftab560\slleading20\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 n1=float(input('qual a altura da parede? '))\
+n2=float(input('qual a largura? '))\
+a=n1*n2\
+t=(a//2)+1\
+print ('a parede tem \{:.2f\} m2 e precisa de \{:.0f\} litros de tinta para pint\'e1-la'.format(a,t))}
