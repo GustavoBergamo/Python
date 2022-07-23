7a9227a880aed64222bd6be72c260478132aaad8
Gustavo bergamo <106891173+GustavoBergamo@users.noreply.github.com>
2022-07-23 17:23:49 -0300

    correções

--- Conversão.py
+++ Conversão.py
@@ -1,10 +1,2 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2513
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 HelveticaNeue;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\deftab560
-\pard\pardeftab560\slleading20\pardirnatural\partightenfactor0
-
-\f0\fs24 \cf0 n=int(input('quantos metros?'))\
-print ('S\'e3o \{\} cent\'edmetros e \{\} mil\'edmetros'.format(n*100,n*1000))}
+n=int(input('quantos metros?'))
+print ('São {} centímetros e {} milímetros'.format(n*100,n*1000))
--- aluguel carro.py
+++ aluguel carro.py
@@ -1,11 +1,3 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2513
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 HelveticaNeue;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\deftab560
-\pard\pardeftab560\slleading20\pardirnatural\partightenfactor0
-
-\f0\fs24 \cf0 d = int(input('Quantos dias o carro foi alugado? '))\
-k = int(input('Quantos km foi percorrido? '))\
+d = int(input('Quantos dias o carro foi alugado? '))
+k = int(input('Quantos km foi percorrido? '))
 print ('Sendo que cada dia corresponde a R$60,00 mais R$0,15 por km, o total foi R$\{:.2f\}'.format((d * 60)+(k * 0.15)))}
