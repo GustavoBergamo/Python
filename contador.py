411f8e0f416d4ef06806422526469c7f5753e3b0
Gustavo bergamo <106891173+GustavoBergamo@users.noreply.github.com>
2022-07-29 10:25:06 -0300

    arrumado contador

--- contador.py
+++ contador.py
@@ -1,6 +1,6 @@
 from time import sleep
+
 def contador(a, b, c):
-    print('=-' * 20)
     print(f'Contagem de {a} até {b} de {c} em {c}')
     if c == 0:
         c = 1
@@ -13,4 +13,8 @@ def contador(a, b, c):
         print(f'{d} ', end='')
         sleep(0.2)
     print('FIM')
-    print('=-' * 20)
+    
+n1=int(input('Você quer contar a partir de qual número? '))
+n2=int(input('Qual será o número final? '))
+n3=int(input('De quantos em quantos números é pra ser contado? '))
+contador(n1, n2, n3)
